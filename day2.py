0f2c129bb7532c179c1217e0f9cd450150e0209a
Sneha-20-7 <snehasarkar20.7@gmail.com>
2025-02-18 07:55:11 +0530

    day2

---
+++ day2.py
@@ -0,0 +1,83 @@
+a = "Sneha"
+A = 8668
+A += 9674576
+A -= 9
+A *= 967
+A /= 9
+e = 'Sarkar'
+f = '''hi'''
+b = 564
+c = 45.86
+g = True
+h = False
+i = None
+j = (17>8)
+bool1 = True
+bool2 = False
+k = "76545687"
+l = input("enter your name :")
+
+
+print(a)
+print(A)
+print(b)
+print(c)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+
+print("The value of A+b is ", A+b)
+print("The value of A-b is ", A-b)
+print("The value of A*b is ", A*b)
+print("The value of A/b is ", A/b)
+print("The value of new A is",A )
+
+print("the value of bool1 and bool2 is", (bool1 and bool2))
+print("the value of bool1 and bool2 is", (bool1 or bool2))
+print("the value of not bool2 is", (not bool2))
+
+k = int(k)
+print(k+7)
+
+print(l)
+l = int(l)
+print(type(l))
+
+print(type(a))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
+
+
+
+
+
+
+#chap 2 practice set
+p = 8785870978776579876545579
+q = 876754534534
+
+print("sum of a and b is", p+q)
+
+p = 8785870978776579876545579
+q = 876754534534
+
+print("the remainder when p is divided by q is", p%q)
+
+m=int(input("enter m : "))
+n=int(input("enter n : "))
+print("The avg of m and n is ", (m+n)/2)
+
+x =int(input("enter x: "))
+print("The square of x is ", x**2)
+
